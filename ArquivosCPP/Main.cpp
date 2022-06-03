16788e9baa9cf1c4827401fd85785de7b0c6d3d3
VictorAlves08 <victoralvees17@gmail.com>
2022-06-03 02:59:25 -0300

    create mapa and coins

--- ArquivosCPP/Main.cpp
+++ ArquivosCPP/Main.cpp
@@ -1,60 +1,63 @@
-#include <allegro5/allegro.h>
 #include <iostream>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
-#define MAP_IMG "images/map.bmp"
-#define PONTO_IMG "images/ponto.png"
+#define TIJOLO_IMG "images/tijolo.png"
+#define PILULA_IMG "images/comida.png"
 
 using namespace std;
+const int LINHAS = 20;
+const int COLUNAS = 31;
+bool EXIT = false;
 
-int mapa(int mod) {
-	int i, j;
-	int maps[24][24];
-	ALLEGRO_BITMAP* ponto = NULL;
+ALLEGRO_BITMAP* tijolo = NULL;
+ALLEGRO_BITMAP* pilula = NULL;
 
-	ponto = al_load_bitmap(PONTO_IMG);
-	if (!ponto) {
-		cout << "Faile to load ponto image file" << endl;
-		return -1;
-	}
 
-	char mp[24][24] =
-	{
-		"11111111111111111111111",
-		"12222222222122222222221",
-		"12111211112121111211121",
-		"12111211112121111211121",
-		"12222222222222222222221",
-		"12111212111111121211121",
-		"12222212222122221222221",
-		"11111211110101111211111",
-		"11111210000000001211111",
-		"11111210111011101211111",
-		"00000000111011100000000",
-		"11111210111011101211111",
-		"11111210111111101211111",
-		"11111210000000001211111",
-		"11111210111111101211111",
-		"12222222222122222222221",
-		"12111211112121111211121",
-		"12221222222022222212221",
-		"11121212111111121212111",
-		"12222212222122221222221",
-		"12111111112121111111121",
-		"12222222222222222222221",
-		"11111111111111111111111",
+
+char mapa[LINHAS][COLUNAS] = {
+  "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  "Xo o |o o o XXXXX o o o| o  X",
+  "XoXXX XXXXX XXXXX XXXXX XXX X",
+  "XoXXX XXXXX XXXXX XXXXX XXXoX",
+  "Xo o o o|o o o o o o|o o o oX",
+  "XoXXXoXX XXXXXXXXXXX XXoXXXoX",
+  "Xo o |XXo o |XXX|o o XXo o oX",
+  "XoXXXoXXXXXX XXX XXXXXXoXXXoX",
+  "X XXXoXX ooo|ooo|ooo XXoXXX X",
+  " o o |XX XXXXX XXXXX XX| o o ",
+  "X XXXoXX XXXXXXXXXXX XXoXXX X",
+  "XoXXXoXX oo |ooo|ooo XXoXXXoX",
+  "X XXXoXXXXXX XXX XXXXXXoXXX X",
+  "Xo o  XXo o oXXX o o XX o o X",
+  "X XXXoXX XXXXXXXXXXX XXoXXX X",
+  "XoXXX| o| o o o o o |o |XXXoX",
+  "X XXXoXXXX XXXXXXXX XXX XXX X",
+  "XoXXXoXXXX o o o o oXXX XXXoX",
+  "X  o |o o  XXXXXXXX o o| o  X",
+  "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+};
+
+
+void criarMapa() {
+	int row, col;
+	for (row = 0; row < LINHAS; row++) {
+		for (col = 0; col < COLUNAS; col++) {
+			if (mapa[row][col] == 'X') {
+				al_draw_bitmap(tijolo, col * 30, row * 30, 0);
+			}
+			else if (mapa[row][col] == 'o') {
+				al_draw_bitmap(pilula, col * 30, row * 30, 0);
+				//if (py / 30 == row && px / 30 == col) {
+				//	mapa[row][col] = '';
+				//};
+			};
+		};
 	};
-
-	for (i = 0; i < 24; i++) {
-		for (j = 0; j < 24; j++) {
-			if (mod)maps[i][j] = mp[i][j];
-			if (maps[i][j] == 2) al_draw_bitmap(ponto, 0, 0, 0);
-		}
-	}
-}
+};
 
 int main() {
-	ALLEGRO_DISPLAY *display = NULL;
-	ALLEGRO_BITMAP *map = NULL;
+	ALLEGRO_DISPLAY* display = NULL;
 
 	if (!al_init()) {
 		cout << "Failed to initialize allegro!" << endl;
@@ -62,29 +65,27 @@ int main() {
 	}
 
 	al_init_image_addon();
-	
-	display = al_create_display(460, 460);
 
+	display = al_create_display(880, 600);
 	if (!display) {
 		cout << "Failed to create the display!" << endl;
 		return -1;
 	}
 
-	map = al_load_bitmap(MAP_IMG);
-	if (!map) {
-		cout << "Faile to load map image file" << endl;
-		return -1;
-	}
+    tijolo = al_load_bitmap(TIJOLO_IMG);
+	pilula = al_load_bitmap(PILULA_IMG);
 
-	al_draw_bitmap(map, 0, 0, 0);
-	mapa(0);
 
-	al_flip_display();
+    while (!EXIT) {
+        criarMapa();
+	    al_flip_display();
+		al_rest(10);
+		/*EXIT = true;*/
+    };
 
-	al_rest(5);
 
 	al_destroy_display(display);
-	al_destroy_bitmap(map);
 	al_uninstall_system();
+
 	return 0;
 }
