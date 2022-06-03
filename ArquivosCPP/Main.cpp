9feb7d653446aa2ff5e407b71d502ab7de08fa64
Davi Rangel <davirangelcamara@hotmail.com>
2022-06-03 12:21:54 -0300

    Adicionando pílulas e mais moedas

--- ArquivosCPP/Main.cpp
+++ ArquivosCPP/Main.cpp
@@ -3,7 +3,8 @@
 #include <allegro5/allegro.h>
 #include <allegro5/allegro_image.h>
 #define TIJOLO_IMG "images/tijolo.png"
-#define PILULA_IMG "images/comida.png"
+#define MOEDA_IMG "images/comida.png"
+#define PILULA_IMG "images/pilula.png"
 
 using namespace std;
 const int LINHAS = 20;
@@ -11,30 +12,31 @@ const int COLUNAS = 31;
 bool EXIT = false;
 
 ALLEGRO_BITMAP* tijolo = NULL;
+ALLEGRO_BITMAP* moeda = NULL;
 ALLEGRO_BITMAP* pilula = NULL;
 
 
 
 char mapa[LINHAS][COLUNAS] = {
   "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
-  "Xo o |o o o XXXXX o o o| o  X",
-  "XoXXX XXXXX XXXXX XXXXX XXX X",
-  "XoXXX XXXXX XXXXX XXXXX XXXoX",
-  "Xo o o o|o o o o o o|o o o oX",
-  "XoXXXoXX XXXXXXXXXXX XXoXXXoX",
-  "Xo o |XXo o |XXX|o o XXo o oX",
-  "XoXXXoXXXXXX XXX XXXXXXoXXXoX",
-  "X XXXoXX ooo|ooo|ooo XXoXXX X",
-  " o o |XX XXXXX XXXXX XX| o o ",
-  "X XXXoXX XXXXXXXXXXX XXoXXX X",
-  "XoXXXoXX oo |ooo|ooo XXoXXXoX",
-  "X XXXoXXXXXX XXX XXXXXXoXXX X",
-  "Xo o  XXo o oXXX o o XX o o X",
-  "X XXXoXX XXXXXXXXXXX XXoXXX X",
-  "XoXXX| o| o o o o o |o |XXXoX",
-  "X XXXoXXXX XXXXXXXX XXX XXX X",
-  "XoXXXoXXXX o o o o oXXX XXXoX",
-  "X  o |o o  XXXXXXXX o o| o  X",
+  "Xoooo|ooooooXXXXXoooooo|ooooX",
+  "XoXXXoXXXXXoXXXXXoXXXXXoXXXoX",
+  "XoXXXoXXXXXoXXXXXoXXXXXoXXXoX",
+  "XoooooooooooooooooooooooooooX",
+  "XoXXXoXXoXXXXXXXXXXXoXXoXXXoX",
+  "XoooooXXoooooXXXoooooXXoooooX",
+  "XoXXXoXXXXXXoXXXoXXXXXXoXXXoX",
+  "XoXXXoXXoooo|ooo|ooooXXoXXXoX",
+  " oooooXXoXXXXXXXXXXXoXXooooo ",
+  "XoXXXoXXoXXXXXXXXXXXoXXoXXXoX",
+  "XoXXXoXXoooooooooooooXXoXXXoX",
+  "XoXXXoXXXXXXoXXXoXXXXXXoXXXoX",
+  "XoooooXXoooooXXXoooooXXoooooX",
+  "XoXXXoXXoXXXXXXXXXXXoXXoXXXoX",
+  "XoXXX|ooooooooooooooooo|XXXoX",
+  "XoXXXoXXXXoXXXXXXXXoXXXoXXXoX",
+  "XoXXXoXXXXooooooooooXXXoXXXoX",
+  "Xoooo|oooooXXXXXXXXoooo|ooooX",
   "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
 };
 
@@ -47,10 +49,13 @@ void criarMapa() {
 				al_draw_bitmap(tijolo, col * 30, row * 30, 0);
 			}
 			else if (mapa[row][col] == 'o') {
-				al_draw_bitmap(pilula, col * 30, row * 30, 0);
+				al_draw_bitmap(moeda, col * 30 + 8, row * 30 + 8, 0);
 				//if (py / 30 == row && px / 30 == col) {
 				//	mapa[row][col] = '';
 				//};
+			}
+			else if (mapa[row][col] == '|') {
+				al_draw_bitmap(pilula, col * 30 + 8, row * 30 + 6, 0);
 			};
 		};
 	};
@@ -73,6 +78,7 @@ int main() {
 	}
 
     tijolo = al_load_bitmap(TIJOLO_IMG);
+	moeda = al_load_bitmap(MOEDA_IMG);
 	pilula = al_load_bitmap(PILULA_IMG);
 
 
