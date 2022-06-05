eb4b5e8fd922c878ce15f565d29451be8b40abab
Matheus Moreira <matt4git@gmail.com>
2022-06-05 19:24:34 -0300

    Revert "try add oop"
    This reverts commit f2663e6270a5d53839d624b266ac0eb6c695617a.

---
+++ ArquivosH/Pilula.h
@@ -0,0 +1,17 @@
+#ifndef PILULA_H
+#define	PILULA_H
+
+// Classe de pilulas que herda da classe base EstruturasMapa
+
+class Pilula
+{
+private:
+	int altura;
+	int comprimento;
+public:
+	Pilula();
+	~Pilula();
+};
+
+#endif // !PILULAS_H
+
---
+++ ArquivosH/Tijolo.h
@@ -0,0 +1,17 @@
+#ifndef TIJOLO_H
+#define TIJOLO_H
+// Classe tijolo que herdará da classe base EstruturasMapa
+
+class Tijolo
+{
+private:
+	int Altura;
+	int Comprimento;
+public:
+	void Desenhar();
+	Tijolo();
+	~Tijolo();
+};
+
+
+#endif // !TIJOLO_H
